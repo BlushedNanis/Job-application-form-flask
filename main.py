c8e88165a400db53bd70be277302002a22e4e120
BlushedNanis <luis_danieltg@outlook.com>
2024-05-10 22:36:31 -0400

    Added database

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from os import getenv
 
+
+load_dotenv()
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = getenv("key")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+db = SQLAlchemy(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+    
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -15,4 +34,8 @@ def index():
         
     return render_template("index.html")
 
-app.run(port=5001, debug=True)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(port=5001, debug=True)
